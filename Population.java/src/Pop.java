541b74270636125d211059e5f9bf9ada08661d9c
Michael Buckle <buckle0112@gmail.com>
2024-04-23 20:48:43 -0400

    Fixed

--- Population.java/src/Pop.java
+++ Population.java/src/Pop.java
@@ -1,24 +1,22 @@
 public class Pop {
-    public class Population {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            double birthRateInSeconds = 7.0;
-            double deathRateInSeconds = 13.0;
-            double newImmigrantInSeconds = 45.0;
+        double birthRateInSeconds = 7.0;
+        double deathRateInSeconds = 13.0;
+        double newImmigrantInSeconds = 45.0;
 
-            double currentPopulation = 312032486;
+        double currentPopulation = 312032486;
 
-            double secondsInYears = 60 * 60 * 24 * 365;
+        double secondsInYears = 60 * 60 * 24 * 365;
 
-            double numBirths = secondsInYears / birthRateInSeconds;
-            double numDeaths = secondsInYears / deathRateInSeconds;
-            double numImmigrants = secondsInYears / newImmigrantInSeconds;
+        double numBirths = secondsInYears / birthRateInSeconds;
+        double numDeaths = secondsInYears / deathRateInSeconds;
+        double numImmigrants = secondsInYears / newImmigrantInSeconds;
 
-            for (int i = 1; i <= 5; i++) {
-                currentPopulation += numBirths + numImmigrants - numDeaths;
-                System.out.println("Year " + i + " = " + (int) currentPopulation);
-            }
-
+        for (int i = 1; i <= 5; i++) {
+            currentPopulation += numBirths + numImmigrants - numDeaths;
+            System.out.println("Year " + i + " = " + (int) currentPopulation);
         }
+
     }
 }
